3cc1831a26b419405c28136680fbcd16dc9bdf06
Lozhkin <dmitry@lozhkin.net>
2016-11-02 13:23:40 +0800

    upd

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.1 beta/15555
 // Filename: INK_material2.ggsk
-// Generated Wed Nov 2 12:22:51 2016
+// Generated Wed Nov 2 13:22:55 2016
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
