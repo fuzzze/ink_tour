a59522df34225a70212a52306fa47e07408932d1
vsrat <vsrat@vsrat.cf>
2024-05-29 08:48:47 +0300

    fix photo titles

--- 2e49822e24b80f9d01f3c76b8edf8460fc42a3cb/skin.js
+++ 2e49822e24b80f9d01f3c76b8edf8460fc42a3cb/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.1/17227
 // Filename: INK_material_2024.ggsk
-// Generated Tue May 28 23:11:49 2024
+// Generated Wed May 29 08:47:54 2024
 
 function pano2vrSkin(player,base) {
 	player.addVariable('currentPan', 0, "1");
