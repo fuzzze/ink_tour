cb8d9eb903cda232ed5a24f1bb4641d175dfb3d4
vsrat <vsrat@vsrat.cf>
2024-06-04 17:24:06 +0800

    tuesday fixes

--- 2e49822e24b80f9d01f3c76b8edf8460fc42a3cb/skin.js
+++ 2e49822e24b80f9d01f3c76b8edf8460fc42a3cb/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.1/17227
 // Filename: INK_material_2024.ggsk
-// Generated Mon Jun 3 15:46:44 2024
+// Generated Tue Jun 4 17:23:42 2024
 
 function pano2vrSkin(player,base) {
 	player.addVariable('currentPan', 0, "1");
